e6488ab6abd90d468f20eb458da68de6cd7b8e41
luckysoo3516 <sumin328030@gmail.com>
2020-03-19 14:50:17 +0900

    사용자 API 테스트 코드추가

--- index.js
+++ index.js
@@ -8,11 +8,47 @@ let users = [
 ];
 
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/users',(req, res) => {
-    res.json(users);
+    req.query.limit = req.query.limit || 10;
+    const limit = parseInt(req.query.limit, 10); // '2'
+    if(Number.isNaN(limit)) {
+        return res.status(400).end();
+    }
+    res.json(users.slice(0, limit));
+});
+app.get('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    //users array에서 id가 1인 객체를 찾아 배열로 반환
+    if(Number.isNaN(id)) return res.status(400).end();
+    //user.id와 같은 user가 없으면 undefined가 리턴된다.
+    const user = users.filter(user => (user.id === id))[0];
+    if(!user) return res.status(404).end();
+    res.json(user);
 });
 
+app.delete('/users/:id', (req, res) => {
+    //경로에 있는 값을 얻으려면 params . string으로 들어옴.
+    const id = parseInt(req.params.id, 10);
+    if(Number.isNaN(id)) return res.status(400).end();
+    users = users.filter(user => user.id !== id);
+    res.status(204).end();
+})
+
+app.post('/users', (req, res) => {
+    const name = req.body.name;
+    if( !name ) return res.status(400).end();
+    
+    const isConflict = users.filter(user => user.name === name).length;
+    if (isConflict) return res.status(409).end();
+    
+    const id = Date.now();
+    const user = { id, name };
+    users.push(user);
+    res.status(201).json(user);
+})
 app.listen(3000, ()=> console.log('Server is running'));
 
 module.exports = app;
