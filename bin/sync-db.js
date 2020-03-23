f02810ce6d7975e89abdb8ce8e3c8c9e02f87685
luckysoo3516 <sumin328030@gmail.com>
2020-03-24 00:18:38 +0900

    테스트 환경 개선 및 db 연동 완료

--- bin/sync-db.js
+++ bin/sync-db.js
@@ -1,7 +1,11 @@
 const models = require('../models');
 
 // models.sequaliz.sync는 내부적으로 비동기. 알아서 처리해줌.
+// force : true이면 기존 테이블을 다 삭제하고 동기화된다.
 module.exports = () => {
-    return models.sequelize.sync({force : true});
-
+    const options = {
+        force : process.env.NODE_ENV === 'test' ? true : false
+    }
+    
+    return models.sequelize.sync(options);
 }
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const user = require('./api/user/index');
 
 // script에서 설정한 NODE_ENV라는 환경변수는 process라는 객체에 들어가게됨.
-if(!process.env.NODE_ENV === 'test'){
+if(process.env.NODE_ENV !== 'test'){
     app.use(morgan('dev'));
 }
 
@@ -13,5 +13,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/users', user); // /users로 들어오는 모든 요청은 user가 담당한다.
-
+app.use('/photos', photo); // api 확장
 module.exports = app;
